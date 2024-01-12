8581c9c856052ecebbf004c807fb3b9060488fdf
AyaTareekAhmmeed <91810219+AyaTareekAhmmeed@users.noreply.github.com>
2024-01-12 20:34:29 +0200

    Add files via upload

---
+++ Querying the Data Warehouse using PostgreSQL (Cubes, Rollups, Grouping Sets and Materialized Views)/Create a Materialized Query Table(MQT).sql
@@ -0,0 +1,24 @@
+CREATE MATERIALIZED VIEW countrystats (country, year, totalbilledamount) AS
+(select country, year, sum(billedamount)
+from "FactBilling"
+left join "DimCustomer"
+on "FactBilling".customerid = "DimCustomer".customerid
+left join "DimMonth"
+on "FactBilling".monthid="DimMonth".monthid
+group by country,year);
+
+
+select year, quartername, sum(billedamount) as totalbilledamount
+from "FactBilling"
+left join "DimCustomer"
+on "FactBilling".customerid = "DimCustomer".customerid
+left join "DimMonth"
+on "FactBilling".monthid="DimMonth".monthid
+group by grouping sets(year, quartername);
+
+
+--Populate/refresh data into the MQT
+REFRESH MATERIALIZED VIEW countrystats;
+
+--Query the MQT.
+select * from countrystats;
